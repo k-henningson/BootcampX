173633da5c4b83751c7c40c91e951a98094b06c2
Kyra Henningson <kyrahenningson@hotmail.com>
2022-09-21 20:54:49 +0000

    node postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(student => {
+    console.log(`${student.name} has an id of ${student.student_id} and was in the ${student.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
